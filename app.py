43221418d2e75b946f134d96438d45ecc1835125
Maria Ramirez <mj161099@gmail.com>
2024-11-02 20:53:07 -0400

    Se añade script app.py.

---
+++ app.py
@@ -0,0 +1,16 @@
+import random
+from kart import Kart
+from mario import Mario
+from luigi import Luigi
+from peach import Peach
+from toad import Toad
+from lakitu import Lakitu
+from eje import Eje
+from animaciones import KartAnimacion
+from carrera import Carrera
+from menu_kart import   Menu
+from barajas import Baraja
+
+menu = Menu("Mario Kart", 1.0)
+
+
