53fb42d675ad3bd0d0dd96693913cfd00b042d4c
Maria Ramirez <mj161099@gmail.com>
2024-11-02 21:07:40 -0400

    Se añade script de uno de los jugadores disponibles en el juego Mario.

---
+++ mario.py
@@ -0,0 +1,30 @@
+from kart import Kart
+from eje import Eje
+from llantas import Llantas
+
+class Mario(Kart, Eje):
+
+    def __init__(self):
+        super().__init__("Mario", 200, 10, "Pirelli", 1, 1000, 1, 50)
+
+    def usar_habilidad_especial(self):
+        super().usar_habilidad_especial()
+        return (f"{self.jugador} activa Super Estrella! Invencibilidad temporal.")
+
+    def ascii_ganador (self):
+        super().ascii_ganador()
+        print ("""
+▒▒▒▒▒▒▒▒▒▄▄▄▄▒▒▒▒▒▒▒
+▒▒▒▒▒▒▄▀▀▓▓▓▀█▒▒▒▒▒▒
+▒▒▒▒▄▀▓▓▄██████▄▒▒▒▒
+▒▒▒▄█▄█▀░░▄░▄░█▀▒▒▒▒
+▒▒▄▀░██▄░░▀░▀░▀▄▒▒▒▒
+▒▒▀▄░░▀░▄█▄▄░░▄█▄▒▒▒
+▒▒▒▒▀█▄▄░░▀▀▀█▀▒▒▒▒▒
+▒▒▒▄▀▓▓▓▀██▀▀█▄▀▀▄▒▒
+▒▒█▓▓▄▀▀▀▄█▄▓▓▀█░█▒▒
+▒▒▀▄█░░░░░█▀▀▄▄▀█▒▒▒
+▒▒▒▄▀▀▄▄▄██▄▄█▀▓▓█▒▒
+▒▒█▀▓█████████▓▓▓█▒▒
+▒▒█▓▓██▀▀▀▒▒▒▀▄▄█▀▒▒
+▒▒▒▀▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ \n""")
