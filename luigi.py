93ea5635907e10d7e516fea8a93decd3190ae257
Maria Ramirez <mj161099@gmail.com>
2024-11-02 21:06:53 -0400

    Se añade script de uno de los jugadores disponibles en el juego Luigi.

---
+++ luigi.py
@@ -0,0 +1,30 @@
+from kart import Kart
+from eje import Eje
+from llantas import Llantas
+
+class Luigi(Kart, Eje):
+
+    def __init__(self):
+        super().__init__("Luigi", 200, 9, "Goodyear", 1, 1100, 1.7, 40)
+
+    def usar_habilidad_especial(self):
+        super().usar_habilidad_especial()
+        return (f"{self.jugador} activa Salto Alto! Puede saltar obstáculos.")
+
+    def ascii_ganador (self):
+        super().ascii_ganador()
+        print("""
+▒▒▒▒▒▒▒▒▒▄▄▄▄▒▒▒▒▒▒▒
+▒▒▒▒▒▒▄▀▀▓▓▓▀█▒▒▒▒▒▒
+▒▒▒▒▄▀▓▓▄██████▄▒▒▒▒
+▒▒▒▄█▄█▀░░▄░▄░█▀▒▒▒▒
+▒▒▄▀░██▄░░▀░▀░▀▄▒▒▒▒
+▒▒▀▄░░▀░▄█▄▄░░▄█▄▒▒▒
+▒▒▒▒▀█▄▄░░▀▀▀█▀▒▒▒▒▒
+▒▒▒▄▀▓▓▓▀██▀▀█▄▀▀▄▒▒
+▒▒█▓▓▄▀▀▀▄█▄▓▓▀█░█▒▒
+▒▒▀▄█░░░░░█▀▀▄▄▀█▒▒▒
+▒▒▒▄▀▀▄▄▄██▄▄█▀▓▓█▒▒
+▒▒█▀▓█████████▓▓▓█▒▒
+▒▒█▓▓██▀▀▀▒▒▒▀▄▄█▀▒▒
+▒▒▒▀▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒\n""")
