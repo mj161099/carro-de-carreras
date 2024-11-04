866f97ea205f01e3672a07df7eb71a1eb1d1c878
Maria Ramirez <mj161099@gmail.com>
2024-11-04 12:16:08 -0400

    Mejoras al codigo llantas: Uso de diccionarios y constantes para evitar la repeticion.

--- llantas.py
+++ llantas.py
@@ -1,43 +1,39 @@
 from excepciones import ImproperTireException
 
 class Llantas:
-    def __init__(self, llantas):
-        self.llantas = llantas
-        self.traccion = self.definir_traccion()
-        self.peso_extra = self.definir_peso_extra()
-        self.gasolina_extra = self.definir_gasolina_extra()
+    MICHELIN = "Michelin"
+    PIRELLI = "Pirelli"
+    GOODYEAR = "Goodyear"
+    BRIDGESTONE = "Bridgestone"
 
-    def definir_traccion(self):
-        if self.llantas == "Michelin":
-            return "Buena"
-        elif self.llantas == "Pirelli":
-            return "Excelente"
-        elif self.llantas == "Goodyear":
-            return "Regular"
-        elif self.llantas == "Bridgestone":
-            return "Regular"
-        else:
+    TRACCIONES = {
+        MICHELIN: "Buena",
+        PIRELLI: "Excelente",
+        GOODYEAR: "Regular",
+        BRIDGESTONE: "Regular"
+    }
+
+    PESO_EXTRA = {
+        MICHELIN: 0.10,
+        PIRELLI: 0.05,
+        GOODYEAR: 0.00,
+        BRIDGESTONE: 0.00
+    }
+
+    GASOLINA_EXTRA = {
+        MICHELIN: 0.00,
+        PIRELLI: 0.00,
+        GOODYEAR: 0.20,
+        BRIDGESTONE: 0.30
+    }
+
+    def __init__(self, llantas):
+        if llantas not in self.TRACCIONES:
             raise ImproperTireException("Tipo de llantas desconocido!")
+        self.llantas = llantas
+        self.traccion = self.TRACCIONES[llantas]
+        self.peso_extra = self.PESO_EXTRA[llantas]
+        self.gasolina_extra = self.GASOLINA_EXTRA[llantas]
 
     def __str__(self):
         return self.llantas
-
-    def definir_peso_extra(self):
-        #Aumenta peso 10%
-        if self.llantas == "Michelin":
-            return 0.10
-        #Aumenta peso 5%
-        elif self.llantas == "Pirelli":
-            return 0.05
-        else:
-            return 0
-
-    def definir_gasolina_extra(self):
-        #Consume gasolina 20%
-        if self.llantas == "Goodyear":
-            return 0.20
-        #Consume gasolina 30%
-        elif self.llantas == "Bridgestone":
-            return 0.30
-        else:
-            return 0
